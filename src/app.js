63de4d6ac208ce13b374482a534744eae0db8a2d
Siddharth NA <siddharth.nagrajann@gmail.com>
2020-08-08 15:33:26 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import HelloWorld from './components/HelloWorld.js'
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+
+ReactDOM.render(<HelloWorld />, document.getElementById('app'))
